d3560b2fa93a0bba98e3ef6acf6e20b1e4687fd7
ElioPeraza <perazaelio@gmail.com>
2025-04-07 21:32:16 -0400

    fix: add build script for Render

--- tecnowinch/vite.config.js
+++ tecnowinch/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // Esto confirma que la salida será en la carpeta 'dist'
+  }
 })
